7240368b8ba20e1688584c3796529a0569849c2a
garyng <garyng.zhongbo@gmail.com>
2020-07-01 02:58:41 +0800

    feat: refactor

--- src/Giti/Program.cs
+++ src/Giti/Program.cs
@@ -1,14 +1,12 @@
 ﻿using LibGit2Sharp;
 using System;
 using System.IO;
-using System.Linq;
 using System.Text.RegularExpressions;
-using System.Threading.Channels;
 using System.Threading.Tasks;
+using Colorify;
+using Colorify.UI;
 using CommandLine;
 using CommandLine.Text;
-using DryIoc;
-using ImTools;
 using Scriban;
 
 namespace Giti
@@ -39,151 +37,69 @@ namespace Giti
 	{
 		static async Task Main(string[] args)
 		{
-			args = new[]
-			{
-				"test-repo",
-				"-t",
-				"GitHeadFriendlyName",
-				"-p",
-				@"[a-zA-Z]+-\d+",
-				"-r",
-				"{{ message }}"
-			};
 			var result = await new Parser(config => config.HelpWriter = null)
 				.ParseArguments<Options>(args)
-				.WithParsedAsync(async options => await (await App.New(options)).Run());
+				.WithParsedAsync(options => Run(options));
 			result.WithNotParsed(errors => Console.WriteLine(
 				HelpText.AutoBuild(result, help =>
 				{
 					help.AddEnumValuesToHelpText = true;
 					return help;
 				})));
-
-			Console.ReadKey();
-		}
-	}
-
-	public class App
-	{
-		public static async Task<App> New(Options options)
-		{
-			var container = new Container();
-
-			container.RegisterInstance(options);
-			container.RegisterDelegate(() => new Repository(Directory.GetCurrentDirectory()), reuse: Reuse.Singleton);
-
-			new[]
-				{
-					typeof(GitHeadCanonicalNameSource), typeof(GitHeadFriendlyNameSource),
-					typeof(RegexExtractor), 
-					typeof(ScribanReplacer)
-				}
-				.ForEach(t => container.Register(t));
-
-			container.Register<App>();
-
-			return container.Resolve<App>();
 		}
 
-		private readonly ISource _source;
-		private readonly IExtractor _extractor;
-		private readonly IReplacer _replacer;
-		private readonly Repository _repo;
-
-		public App(ISource source, IExtractor extractor, IReplacer replacer, Lazy<Repository> repo)
+		public static async Task Run(Options option)
 		{
-			_source = source;
-			_extractor = extractor;
-			_replacer = replacer;
-			_repo = repo.Value;
-		}
+			var console = new Format(new ThemeDark());
 
-		
+			using var repo = TryCreateRepo();
+			if (repo is null)
+			{
+				Error("Not a valid git repo");
+				return;
+			}
 
-		public async Task Run()
-		{
-			var match = await _extractor.Extract(await _source.Get());
-			_replacer.Replace(new
+			var source = option.SourceType switch
+			{
+				SourceTypes.GitHeadFriendlyName => repo.Head.FriendlyName,
+				SourceTypes.GitHeadCanonicalName => repo.Head.CanonicalName,
+				_ => throw new ArgumentOutOfRangeException()
+			};
+			var match = Regex.Match(source, option.Pattern).Value;
+			if (string.IsNullOrEmpty(match))
+			{
+				Warn($"Pattern '{option.Pattern}' matched nothing");
+				return;
+			}
+
+			Ok($"Matched '{match}' from '{source}'");
+
+			var originalMessage = await File.ReadAllTextAsync(option.CommitMessageFile);
+			var template = Template.Parse(option.Template);
+			var finalMessage = template.Render(new
 			{
 				match,
+				message = originalMessage,
 			});
+			await File.WriteAllTextAsync(option.CommitMessageFile, finalMessage);
 
-		}
-	}
 
-	public class GitHeadFriendlyNameSource : ISource
-	{
-		private readonly Repository _repo;
+			Repository? TryCreateRepo()
+			{
+				var curDir = Directory.GetCurrentDirectory();
+				try
+				{
+					return new Repository(curDir);
+				}
+				catch (Exception)
+				{
+					return null;
+				}
+			}
 
-		public GitHeadFriendlyNameSource(Lazy<Repository> repo)
-		{
-			_repo = repo.Value;
-		}
-
-		public async Task<string> Get()
-		{
-			return _repo.Head.FriendlyName;
-		}
-	}
-
-	public class GitHeadCanonicalNameSource : ISource
-	{
-		private readonly Repository _repo;
-
-		public GitHeadCanonicalNameSource(Lazy<Repository> repo)
-		{
-			_repo = repo.Value;
-		}
-
-		public async Task<string> Get()
-		{
-			return _repo.Head.CanonicalName;
-		}
-	}
-
-	public interface ISource
-	{
-		Task<string> Get();
-	}
-
-	public interface IExtractor
-	{
-		Task<string> Extract(string source);
-	}
-
-	public class RegexExtractor : IExtractor
-	{
-		private Regex _regex;
-
-		public RegexExtractor(string pattern)
-		{
-			_regex = new Regex(pattern, RegexOptions.Compiled);
-		}
-
-		public async Task<string> Extract(string source)
-		{
-			return _regex.Match(source).Value;
-		}
-	}
-
-	public interface IReplacer
-	{
-		Task<string> Replace(object model);
-	}
-
-	public class ScribanReplacer : IReplacer
-	{
-		private readonly string _template;
-
-		public ScribanReplacer(string template)
-		{
-			_template = template;
-		}
-
-		public async Task<string> Replace(object model)
-		{
-			var parsed = Template.Parse(_template);
-			return await parsed.RenderAsync(model);
+			void Warn(string text) => console.Write(text, Colors.txtWarning);
+			void Ok(string text) => console.Write(text, Colors.txtSuccess);
+			void Error(string text) => console.Write(text, Colors.txtDanger);
 		}
 	}
 }
